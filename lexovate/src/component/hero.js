25a81c0c5b928c973ccde2b424016de7adc827b7
hasbif <hasbisenpai@gmail.com>
2025-02-08 22:00:16 +0700

    modal and copy button

--- lexovate/src/component/hero.js
+++ lexovate/src/component/hero.js
@@ -1,9 +1,17 @@
 "use client";
 
-import { motion, useAnimation, useScroll, useTransform } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { AnimatePresence, motion, useAnimation, useScroll, useTransform } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 export default function Hero() {
+  const [showToast, setShowToast] = useState(false);
+
+  const handleClick = () => {
+    setShowToast(true);
+    navigator.clipboard.writeText(key);
+    setTimeout(() => setShowToast(false), 500);
+  };
+
   const { scrollY } = useScroll();
   const yPosition = useTransform(scrollY, [0, 400], [0, 100]);
   const initDelay = 1
@@ -22,7 +30,7 @@ export default function Hero() {
 
       await controls.start({
         y: 0,
-        transition: { duration: 0.5, ease: [0.2, 0, 0.2, 1]},
+        transition: { duration: 0.5, ease: [0.2, 0, 0.2, 1] },
       });
 
       await controls.start({
@@ -34,16 +42,17 @@ export default function Hero() {
     sequence();
   }, [controls]);
 
+  const key = "9kEcnFj9vwjUkjdsL89fGhKloP76qseTuv7"
 
   return (
     <section id="home" aria-labelledby="hero" className="h-screen w-full flex flex-col items-center justify-end relative">
       <motion.video
-      ref={videoRef}
+        ref={videoRef}
         className="fixed top-0 left-0 w-full h-full object-cover z-[-1]"
         src={"https://s3-figma-videos-production-sig.figma.com/video/TEAM/1170563733121532378/fe1b78e8671705da2b9f93d8b3e7fd6d6c4edc14?Expires=1739750400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=P0Ek6-wJK2ANP2XyymBJk5hPKY-kFflN7AxivUZfL5Z4~2gNcE-VIfzcASINkAd6cp0trWhQ9aR--oTHf0V6bY9I8BPkt9QAvyl~~TXNYRa99t5rYjruw3FBzFlqJfvN3M-jMwq21pDWc81vSPbjpKQs475q2knV9bA~szaswf2itO0penTmIhiaQTFvr0YIR1VAhDUZPFBmwqFjlELnNBneZlyTZgrr1Sj6N2glHjkflhPaAg1xS4FiTexb0bNu113Uvt97vrCFL8WofepyhjSwe82Znhuz-FA-kvwUe0DcEr9ORQMOWQdcy0T~Jp4SZY8KmibU93Rb6oPphZv31A__"}
         autoPlay
         muted
-        loop
+        // loop
         initial={{ scale: 0.5, y: '100%', clipPath: "inset(20% 15% 20% 15%)" }}
         animate={controls}
       />
@@ -57,16 +66,48 @@ export default function Hero() {
           transition={{ delay: initDelay + 0.15, duration: 0.7, ease: "easeOut" }}
         >
           <div className="rounded-lg flex items-center justify-between w-full max-w-[40rem] bg-white/15 mb-10">
-            <p className="text-sm px-4 py-3">9kEcnFj9vwjUkjdsL89fGhKloP76qseTuv7</p>
-            <div className="flex gap-2.5 rounded-r-lg items-center bg-white/15 px-4 py-3">
-              <img src="/icons/copy-icon.svg" />
-              <span>Copy CA</span>
+            <p className="text-sm px-4 py-3">{key}</p>
+            <div>
+              <motion.button
+                whileHover="hover"
+                className="relative overflow-hidden px-4 py-3 bg-white/15 rounded-r-lg"
+                onClick={handleClick}
+              >
+                <motion.div
+                  variants={{
+                    hover: { x: "100%" }
+                  }}
+                  transition={{ duration: 0.2, ease: "easeInOut" }}
+                  className="absolute inset-0 bg-[linear-gradient(90deg,transparent,#fff,transparent)]"
+                  style={{
+                    x: "-100%"
+                  }}
+                />
+                <span className="relative z-10 flex gap-2.5 items-center">
+                  <img src="/icons/copy-icon.svg" />
+                  <span>Copy CA</span>
+                </span>
+              </motion.button>
+              <AnimatePresence>
+                {showToast && (
+                  <motion.div
+                    className="absolute top-[-50px] bg-white/15 px-4 py-2 rounded-md shadow-lg text-center"
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -20 }}
+                    transition={{ duration: 0.4, ease: "easeInOut" }}
+                  >
+                    Copied To Clipboard
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </div>
           </div>
+
         </motion.div>
         <motion.div
-          initial={{ scale: 0.5,y: 30 ,opacity: 0 }}
-          animate={{ scale: 1,y: 0, opacity: 1 }}
+          initial={{ scale: 0.5, y: 30, opacity: 0 }}
+          animate={{ scale: 1, y: 0, opacity: 1 }}
           transition={{ delay: initDelay, duration: 0.25, ease: "easeOut" }}
         >
           <h4 className="font-semibold mb-2">Empowering You to Build Smarter DeFi Agents -</h4>
--- lexovate/src/component/modal.js
+++ lexovate/src/component/modal.js
@@ -32,10 +32,9 @@ const Modal = ({ isOpen, onClose }) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, duration: 0.1, ease: "easeInOut" }}
           exit={{ opacity: 0 }}
-          className="fixed top-0 left-0 h-screen flex items-center justify-center"
+          className="fixed top-0 left-0 h-screen w-screen z-50 bg-black/70 flex items-center justify-center"
           onClick={handleOverlayClick}
           ref={overlayRef}
-          style={{ width: '100vw', background: 'rgba(0, 0, 0, 0.7)', zIndex: 50 }}
         >
           <motion.div
             initial={{ opacity: 0, y: '100vh' }}
@@ -48,7 +47,20 @@ const Modal = ({ isOpen, onClose }) => {
             <div className="bg-white rounded-2xl p-6 h-fit max-w-xl">
               <div className="flex relative items-center justify-center mb-6">
                 <h6 className="text-xl font-semibold text-background">GET EARLY ACCESS</h6>
-                <button className="absolute right-0" onClick={onClose}><img src="/icons/close-icon.svg" alt="close" /></button>
+                <motion.button
+                  whileHover="hover"
+                  className="absolute right-0"
+                  onClick={onClose}
+                >
+                  <motion.img
+                    src="/icons/close-icon.svg"
+                    alt="close"
+                    variants={{
+                      hover: { rotate: 180 },
+                    }}
+                    transition={{ type: "spring", stiffness: 100, damping: 20 }}
+                  />
+                </motion.button>
               </div>
               <p className="text-center leading-6 mb-8"
                 style={{ color: "#454343" }}
@@ -56,14 +68,27 @@ const Modal = ({ isOpen, onClose }) => {
                 Thank you for your interest in LexovateAI ! We're currently developing the beta version. Please enter your email below to gain access to our exclusive demo as soon as it's ready!
               </p>
               <form>
-                <input placeholder="Enter your email address" className="w-full rounded-lg text-background"
-                  style={{
-                    background: "#FAFAFA",
-                    marginBottom: "1.5rem",
-                    padding: "1rem"
-                  }}
+                <input placeholder="Enter your email address" className="w-full rounded-lg text-background p-4 mb-6 bg-[#FAFAFA] focus:outline-primary active:outline-primary-primary"
                   required type="email" />
-                <button type="submit" className="w-full bg-gradient-to-r from-[#ED6C4E] via-[#E24391] to-[#6E53E8] rounded-lg font-medium" style={{ padding: "0.625rem" }}>SUBMIT</button>
+                <motion.button
+                  whileHover="hover"
+                  className="relative overflow-hidden w-full bg-gradient-to-r from-[#ED6C4E] via-[#E24391] to-[#6E53E8] rounded-lg font-medium p-2.5"
+                  type="submit"
+                >
+                  <motion.div
+                    variants={{
+                      hover: { x: "100%" }
+                    }}
+                    transition={{ duration: 0.2, ease: "easeInOut" }}
+                    className="absolute inset-0 bg-[linear-gradient(90deg,transparent,#fff,transparent)]"
+                    style={{
+                      x: "-100%"
+                    }}
+                  />
+                  <span className="relative z-10">
+                    SUBMIT
+                  </span>
+                </motion.button>
               </form>
             </div>
           </motion.div>
